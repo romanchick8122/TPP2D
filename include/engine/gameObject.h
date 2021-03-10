f35bf27bb2256f3fd8c4b59cc1dca10cd5816c2b
Raman Svistunou <roma-svistunov@tut.by>
2021-03-10 23:46:38 +0300

    Update gameObject.h

--- include/engine/gameObject.h
+++ include/engine/gameObject.h
@@ -5,6 +5,7 @@ struct renderParams {
     sf::Vector2f coordShift;
 };
 class gameObject {
+  public:
     virtual void tick() = 0;
     virtual void lateTick() = 0;
     virtual void render(const renderParams& params) = 0;
