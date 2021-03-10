9795cbacc43195263dbeb5a2054ee027a6a46322
Raman Svistunou <roma-svistunov@tut.by>
2021-03-10 23:41:32 +0300

    gameObject abstraction

---
+++ include/engine/gameObject.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+struct renderParams {
+    sf::RenderWindow targetWindow;
+    sf::Vector2f coordShift;
+};
+class gameObject {
+    virtual void tick() = 0;
+    virtual void lateTick() = 0;
+    virtual void render(const renderParams& params) = 0;
+};
